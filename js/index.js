c111c18e3b215621f63e8512ee2bf0c8dfa178f6
aelichung <aelichung@gmail.com>
2018-04-17 13:07:27 -0700

    complete contact page, add links to footer logos, stop bounce on comic self hover

--- js/index.js
+++ js/index.js
@@ -3,35 +3,45 @@ $(function() {
     $('.hamburger').toggleClass('arrow');
     $('.click-show').toggle('fast');
   });
+  var timer;
   $('.comic-pop-out-right').hover(
     function() {
-      $('.bottom-right').animate(
-        {
-          right: '5%'
-        },
-        1000,
-        function() {}
-      );
-      $(this)
-        .find('.hover-show')
+      timer = setTimeout(function() {
+        $('.bottom-right')
+          .stop(true, true)
+          .animate(
+            {
+              right: '5%'
+            },
+            1000,
+            function() {}
+          );
+        $('.comic-pop-out-right')
+          .find('.hover-show')
+          .stop(true, true)
+          .animate(
+            {
+              opacity: '1'
+            },
+            1000,
+            function() {}
+          );
+      }, 150);
+    },
+    function() {
+      clearTimeout(timer);
+      $('.bottom-right')
+        .stop(true, true)
         .animate(
           {
-            opacity: '1'
+            right: '-2em'
           },
           1000,
           function() {}
         );
-    },
-    function() {
-      $('.bottom-right').animate(
-        {
-          right: '-2em'
-        },
-        1000,
-        function() {}
-      );
-      $(this)
+      $('.comic-pop-out-right')
         .find('.hover-show')
+        .stop(true, true)
         .animate(
           {
             opacity: '0'
@@ -41,35 +51,46 @@ $(function() {
         );
     }
   );
+
   $('.comic-pop-out-left').hover(
     function() {
-      $('.bottom-left').animate(
-        {
-          left: '1em'
-        },
-        1000,
-        function() {}
-      );
-      $(this)
-        .find('.hover-show')
+      timer = setTimeout(function() {
+        $('.bottom-left')
+          .stop(true, true)
+          .animate(
+            {
+              left: '1em'
+            },
+            1000,
+            function() {}
+          );
+        $('.comic-pop-out-left')
+          .find('.hover-show')
+          .stop(true, true)
+          .animate(
+            {
+              opacity: '1'
+            },
+            1000,
+            function() {}
+          );
+      }, 150);
+    },
+    function() {
+      clearTimeout(timer);
+
+      $('.bottom-left')
+        .stop(true, true)
         .animate(
           {
-            opacity: '1'
+            left: '-6em'
           },
           1000,
           function() {}
         );
-    },
-    function() {
-      $('.bottom-left').animate(
-        {
-          left: '-6em'
-        },
-        1000,
-        function() {}
-      );
-      $(this)
+      $('.comic-pop-out-left')
         .find('.hover-show')
+        .stop(true, true)
         .animate(
           {
             opacity: '0'
