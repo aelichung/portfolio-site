9cc7f70573d81bdf3986e255e2fb24dd1c5bb3fa
aelichung <aelichung@gmail.com>
2018-06-18 17:27:00 -0700

    add viewport meta tag to all html files, change scale of houseplant image

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ $(function() {
   $('.comic-pop-out-right').hover(
     function() {
       timer = setTimeout(function() {
-        $('.bottom-right')
+        $('.comic-pop-out-right')
           .stop(true, true)
           .animate(
             {
-              right: '5%'
+              right: '5vw'
             },
             1000,
             function() {}
@@ -26,11 +26,11 @@ $(function() {
     },
     function() {
       clearTimeout(timer);
-      $('.bottom-right')
+      $('.comic-pop-out-right')
         .stop(true, true)
         .animate(
           {
-            right: '-2em'
+            right: '-1vw'
           },
           1000,
           function() {}
@@ -48,52 +48,52 @@ $(function() {
     }
   );
 
-  $('.comic-pop-out-left').hover(
-    function() {
-      timer = setTimeout(function() {
-        $('.bottom-left')
-          .stop(true, true)
-          .animate(
-            {
-              left: '1em'
-            },
-            1000,
-            function() {}
-          );
-        $('.comic-pop-out-left')
-          .find('.hover-show')
-          .stop(true, true)
-          .animate(
-            {
-              opacity: '1'
-            },
-            1000,
-            function() {}
-          );
-      }, 150);
-    },
-    function() {
-      clearTimeout(timer);
-
-      $('.bottom-left')
-        .stop(true, true)
-        .animate(
-          {
-            left: '-6em'
-          },
-          1000,
-          function() {}
-        );
-      $('.comic-pop-out-left')
-        .find('.hover-show')
-        .stop(true, true)
-        .animate(
-          {
-            opacity: '0'
-          },
-          1000,
-          function() {}
-        );
-    }
-  );
+  // $('.comic-pop-out-left').hover(
+  //   function() {
+  //     timer = setTimeout(function() {
+  //       $('.bottom-left')
+  //         .stop(true, true)
+  //         .animate(
+  //           {
+  //             left: '1em'
+  //           },
+  //           1000,
+  //           function() {}
+  //         );
+  //       $('.comic-pop-out-left')
+  //         .find('.hover-show')
+  //         .stop(true, true)
+  //         .animate(
+  //           {
+  //             opacity: '1'
+  //           },
+  //           1000,
+  //           function() {}
+  //         );
+  //     }, 150);
+  //   },
+  //   function() {
+  //     clearTimeout(timer);
+  //
+  //     $('.bottom-left')
+  //       .stop(true, true)
+  //       .animate(
+  //         {
+  //           left: '-6em'
+  //         },
+  //         1000,
+  //         function() {}
+  //       );
+  //     $('.comic-pop-out-left')
+  //       .find('.hover-show')
+  //       .stop(true, true)
+  //       .animate(
+  //         {
+  //           opacity: '0'
+  //         },
+  //         1000,
+  //         function() {}
+  //       );
+  //   }
+  // );
 });
