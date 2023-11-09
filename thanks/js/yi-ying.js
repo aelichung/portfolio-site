04fce27963be3b4d7f8c2bd687a32d228840f31e
Anne-Elise Chung <aelichung@gmail.com>
2023-11-09 10:48:02 -0800

    delete thank you notes

--- thanks/js/yi-ying.js
+++
@@ -1,8 +0,0 @@
-const elements = document.querySelectorAll('animate');
-
-function updateDuration(val) {
-  var newVal = Math.abs(val) + 'ms';
-  Array.prototype.forEach.call(elements, function(el, i) {
-    el.setAttribute('dur', newVal);
-  });
-}
